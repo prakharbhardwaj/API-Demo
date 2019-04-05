c78a72588771846fe06a956eb1851fc7123b1584
Prakhar Prakash Bhardwaj <prkhrbhardwaj6@gmail.com>
2019-04-05 10:51:57 +0530

    adding swift files

---
+++ APIDemo/ReachabilityManager.swift
@@ -0,0 +1,88 @@
+//
+//  ReachabilityManager.swift
+//  APIDemo
+//
+//  Created by Prakhar Prakash Bhardwaj on 02/04/19.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+import UIKit
+import Reachability
+
+public protocol NetworkStatusListener : class {
+    func networkStatusDidChange(status: Reachability.Connection)
+}
+
+class ReachabilityManager: NSObject {
+    
+    static let shared = ReachabilityManager()  // 2. Shared instance
+    
+    // 3. Boolean to track network reachability
+    var isNetworkAvailable : Bool {
+        return reachabilityStatus != .none
+    }
+    
+    // 4. Tracks current NetworkStatus (notReachable, reachableViaWiFi, reachableViaWWAN)
+    var reachabilityStatus: Reachability.Connection = .none
+    
+    // 5. Reachibility instance for Network status monitoring
+    let reachability = Reachability()!
+    
+    var listeners = [NetworkStatusListener]()
+    
+    
+    
+    
+    /// Called whenever there is a change in NetworkReachibility Status
+    ///
+    /// — parameter notification: Notification with the Reachability instance
+    @objc func reachabilityChanged(notification: Notification) {
+        let reachability = notification.object as! Reachability
+        switch reachability.connection {
+        case .none:
+            debugPrint("Network became unreachable")
+        case .wifi:
+            debugPrint("Network reachable through WiFi")
+        case .cellular:
+            debugPrint("Network reachable through Cellular Data")
+        }
+        
+        for listener in listeners {
+            listener.networkStatusDidChange(status: reachability.connection)
+        }
+    }
+    
+    
+    /// Starts monitoring the network availability status
+    func startMonitoring() {
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.reachabilityChanged),
+                                               name: Notification.Name.reachabilityChanged,
+                                               object: reachability)
+        do{
+            try reachability.startNotifier()
+        }catch{
+            debugPrint("Could not start reachability notifier")
+        }
+    }
+    
+    /// Stops monitoring the network availability status
+    func stopMonitoring(){
+        reachability.stopNotifier()
+        NotificationCenter.default.removeObserver(self, name: Notification.Name.reachabilityChanged,
+                                                  object: reachability)
+    }
+    
+    func addListener(listener: NetworkStatusListener){
+        listeners.append(listener)
+    }
+    
+    /// Removes a listener from listeners array
+    ///
+    /// - parameter delegate: the listener which is to be removed
+    func removeListener(listener: NetworkStatusListener){
+        listeners = listeners.filter{ $0 !== listener}
+    }
+}
+
---
+++ APIDemo/WebViewController.swift
@@ -0,0 +1,93 @@
+//
+//  WebViewController.swift
+//  APIDemo
+//
+//  Created by Prakhar Prakash Bhardwaj on 28/03/19.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+import UIKit
+import WebKit
+import Reachability
+
+class WebViewController: UIViewController, WKNavigationDelegate {
+    
+    @IBOutlet weak var practiceVideo: WKWebView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        ReachabilityManager.shared.addListener(listener: self)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        ReachabilityManager.shared.removeListener(listener: self)
+    }
+    
+    func loadVideo() {
+        guard let videoURL = URL(string: "https://www.tripvenza.com/dashboard/")
+            else { return }
+        practiceVideo.load( URLRequest(url: videoURL) )
+        practiceVideo.navigationDelegate = self
+    }
+    
+    func showActivityIndicator(show: Bool) {
+        if show {
+            activityIndicator.startAnimating()
+        } else {
+            activityIndicator.stopAnimating()
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        showActivityIndicator(show: false)
+    }
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        showActivityIndicator(show: true)
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        showActivityIndicator(show: false)
+    }
+}
+
+extension WebViewController: NetworkStatusListener {
+    
+    func networkStatusDidChange(status: Reachability.Connection) {
+        
+        switch status {
+        case .none:
+            debugPrint("ViewController: Network became unreachable")
+        case .wifi:
+            debugPrint("ViewController: Network reachable through WiFi")
+        case .cellular:
+            debugPrint("ViewController: Network reachable through Cellular Data")
+        }
+        
+        // Update login button Enable/Disable status
+        //        loginButton.isEnabled = !(status == .notReachable)
+        if (status == .none) {
+            let alert = UIAlertController(title: "Oops!", message: "No Internet Connection", preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "OK", style: .default)
+            alert.addAction(alertAction)
+//            present(alert, animated: true, completion:nil)
+            if let presented = self.presentedViewController {
+                presented.removeFromParent()
+            }
+            
+            if presentedViewController == nil {
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+            
+        else {
+            loadVideo()
+        }
+    }
+}
